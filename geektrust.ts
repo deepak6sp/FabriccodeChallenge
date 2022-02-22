a558f4d0e8bc24d27ead7f38a3eb7ca98c8ca5a8
Deepak Prakash <deepak@Deepaks-MacBook-Pro.local>
2022-02-22 12:28:16 +0530

    WIP6

--- geektrust.ts
+++ geektrust.ts
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-import { readFileAndOutput } from "./src/loan";
+import { readFileAndOutput } from "./src";
 
 readFileAndOutput()
 
